16e50a0f5f0124980bdb5be939cc8ac3735992eb
fadeandblossom-debug <fadeandblossom@gmail.com>
2026-03-26 23:30:20 +0800

    Update retry.js

--- functions/retry.js
+++ functions/retry.js
@@ -1,6 +1,5 @@
 export async function onRequest(context) {
-  const request = context.request;
-  const url = new URL(request.url);
+  const url = new URL(context.request.url);
   const sid = url.searchParams.get('sid') || '';
 
   if (!sid) {
@@ -11,18 +10,14 @@ export async function onRequest(context) {
   }
 
   const GAS_URL = 'https://script.google.com/macros/s/AKfycbzNvAeEywVCgVGieJFZqgdutS_l4pjbNH4K9CIF8zgtybCapUexWCb9R3PLAM2qFCldnA/exec';
-  const apiUrl = GAS_URL + '?sid=' + encodeURIComponent(sid);
 
-  // 照官方範例：重新建立 Request，並設定 Origin header
-  const newRequest = new Request(apiUrl, request);
-  newRequest.headers.set('Origin', new URL(apiUrl).origin);
+  const gasResponse = await fetch(GAS_URL + '?sid=' + encodeURIComponent(sid));
+  const text = await gasResponse.text();
 
-  const gasResponse = await fetch(newRequest);
-
-  // 照官方範例：重新建立 Response 才能修改 headers
-  const response = new Response(gasResponse.body, gasResponse);
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Content-Type', 'application/json');
-
-  return response;
+  return new Response(text, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
 }
