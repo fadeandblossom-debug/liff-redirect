3f974a83ce38609ff1cb329bbaea9101f3570c0b
fadeandblossom-debug <fadeandblossom@gmail.com>
2026-05-06 18:10:23 +0800

    Create test-gas.js

---
+++ functions/test-gas.js
@@ -0,0 +1,14 @@
+export async function onRequestGet(context) {
+  const GAS_URL = 'https://script.google.com/macros/s/AKfycbzb-IRbn6OoJJDHsv8lREJjZxC9ASfSJnIqjY_bK6pKKXc7kZcmnt1Ke4kD1T7p85GfoQ/exec';
+  try {
+    const res = await fetch(GAS_URL + '?orderId=WOG0lYk', {
+      redirect: 'follow'
+    });
+    const text = await res.text();
+    return new Response(text, {
+      headers: { 'Content-Type': 'text/html' }
+    });
+  } catch (err) {
+    return new Response('Error: ' + err.message, { status: 500 });
+  }
+}
